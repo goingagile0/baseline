9bdcb18eec3d466022e9490b73c63172860eef73
goingagile0 <goingagile0@gmail.com>
2018-01-16 22:37:27 -0600

    Add project files.

---
+++ ACME.Web/Greetings/GreetingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ACME.Web.Greetings
+{
+    [Produces("application/json")]
+    [Route("api/Greeting")]
+    public class GreetingController : Controller
+    {
+        public string Get()
+        {
+            return "hello, world!";
+        }
+    }
+}
